d2bf12324635a40b54c2d1942f503c068810e637
eet-binod-maharjan <binod@e-et-t.com>
2020-03-02 15:57:37 +0545

    validation added in Author

--- tests/Feature/AuthorControllerTest.php
+++ tests/Feature/AuthorControllerTest.php
@@ -21,15 +21,32 @@ class AuthorControllerTest extends TestCase
     public function test_an_author_can_be_created()
     {
         $this->withoutExceptionHandling();
-        $response = $this->post('/author', [
-            'name' => 'John Doe',
-            'dob' => '2020/01/01'
-        ]);
+        $response = $this->post('/author',$this->data());
         $this->assertCount(1, $author = Author::all());
         $this->assertInstanceOf(Carbon::class, $author->first()->dob);
         $this->assertEquals('2020/01/01', $author->first()->dob->format('Y/m/d'));
         $response->assertRedirect('author');
 
+    }
+
+    public function test_a_name_is_required()
+    {
+        $response = $this->post('/author', array_merge( $this->data(), ['name'=>""]));
+        $response->assertSessionHasErrors('name');
+    }
+
+    public function test_a_dob_is_required()
+    {
+        $response = $this->post('/author', array_merge( $this->data(), ['dob'=>""]));
+        $response->assertSessionHasErrors('dob');
+    }
+
+    private function data()
+    {
+        return [
+            'name' => 'John Doe',
+            'dob' => '2020/01/01'
+        ];
     }
 
 }
